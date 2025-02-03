a5eb86cb67b4c18fbd7068ddf4687f58d6823678
EnderBrah18 <jvictorguimadias@gmail.com>
2025-02-03 14:23:52 -0300

    Added Gun with shoot Limit
    Adicionada arma com limite de disparos usando o novo Input System da Unity

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -13,20 +13,8 @@ public class GunBase : MonoBehaviour
 
     public KeyCode keyCode = KeyCode.Z;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(keyCode))
-        {
-            _currentCoroutine = StartCoroutine(StartShoot());
-        }
-        else if (Input.GetKeyUp(keyCode))
-        {
-            if(_currentCoroutine != null) 
-                StopCoroutine(_currentCoroutine);
-        }
-    }
 
-    IEnumerator StartShoot()
+    protected virtual IEnumerator ShootCoroutine()
     {
         while (true)
         {
@@ -42,4 +30,18 @@ public class GunBase : MonoBehaviour
         projectile.transform.rotation = positionToShoot.rotation;
         
     }
+
+    public void StartShoot()
+    {
+        StopShoot();
+        _currentCoroutine = StartCoroutine(ShootCoroutine());
+    }
+
+    public void StopShoot()
+    {
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
+    }
+
+
 }
