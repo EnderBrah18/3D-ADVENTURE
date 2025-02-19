4c11ec44a93e0d7194d063f1914629372feb4717
EnderBrah18 <jvictorguimadias@gmail.com>
2025-02-19 18:29:58 -0300

    Added VFX

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -13,6 +13,9 @@ namespace Enemy
    {
 
         public Collider collider;
+        public FlashColor flashColor;
+        public ParticleSystem particleSystem;
+
         public float startLife = 10f;
 
         [SerializeField] private float _currentLife;
@@ -58,6 +61,8 @@ namespace Enemy
 
         public void OnDamage(float f)
         {
+            if (flashColor != null) flashColor.Flash();
+            if (particleSystem != null) particleSystem.Emit(15);
             _currentLife -= f;
 
             if (_currentLife <= 0)
--- Assets/Scripts/FlashColor.cs
+++ Assets/Scripts/FlashColor.cs
@@ -7,10 +7,24 @@ public class FlashColor : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
 
+    [Header("Setup")]
+    public Color color = Color.red;
+    public float duration = .1f;
+
+    private Color defaultColor;
+
+    private Tween _currTween;
+
+    private void Start()
+    {
+        defaultColor = meshRenderer.material.GetColor("_EmissionColor");
+    }
+
     [NaughtyAttributes.Button]
 
-    private void Flash()
+    public void Flash()
     {
-        meshRenderer.material.DOColor(Color.red, "_EmissionColor", .2f).SetLoops(2, LoopType.Yoyo);
+        if(!_currTween.IsActive())
+        _currTween = meshRenderer.material.DOColor(Color.red, "_EmissionColor", .2f).SetLoops(2, LoopType.Yoyo);
     }
 }
