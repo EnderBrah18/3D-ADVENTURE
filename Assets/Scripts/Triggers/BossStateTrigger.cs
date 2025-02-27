3e55e64851b5e3996767128c50e2ac292694e8b3
EnderBrah18 <jvictorguimadias@gmail.com>
2025-02-27 19:01:08 -0300

    Added ScreenShake

--- Assets/Scripts/Triggers/BossStateTrigger.cs
+++ Assets/Scripts/Triggers/BossStateTrigger.cs
@@ -6,17 +6,37 @@ using Boss;
 public class BossStateTrigger : MonoBehaviour
 {
     public string PlayerTag = "Player";
-    
+
+    public GameObject bossCamera;
+
     public BossBase bossBase;
     private bool hasTriggered = false;
+
+    private void Awake()
+    {
+        bossCamera.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!hasTriggered && other.CompareTag(PlayerTag))
         {
             bossBase.SwitchState(BossAction.INIT);
+            TurnCameraOn();
             hasTriggered = true;
             gameObject.SetActive(false);
         }
     }
+
+    private void TurnCameraOn()
+    {
+        bossCamera.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, transform.localScale.y);
+    }
+
 }
