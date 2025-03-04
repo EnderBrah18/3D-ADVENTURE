0c13dde98411a5e002389310aa900043d3db69c2
EnderBrah18 <jvictorguimadias@gmail.com>
2025-03-04 15:27:44 -0300

    Added DestructableItems drops

--- Assets/Scripts/Items/DestructableItem/DestructableItemBase.cs
+++ Assets/Scripts/Items/DestructableItem/DestructableItemBase.cs
@@ -10,6 +10,10 @@ public class DestructableItemBase : MonoBehaviour
     public float shakeDuration = .1f;
     public int shakeForce = 5;
 
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
+
     private void OnValidate()
     {
         if(healthBase != null) healthBase = GetComponent<healthBase>();
@@ -23,10 +27,30 @@ public class DestructableItemBase : MonoBehaviour
 
     private void OnDamage(healthBase h)
     {
-        transform.DOShakeScale(shakeDuration, Vector3.up, shakeForce);
+        transform.DOShakeScale(shakeDuration, Vector3.up/2, shakeForce);
+        DropGroupOfCoins();
     }
 
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
 
+    }
 
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for (int i = 0; i < dropCoinsAmount; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 
 }
