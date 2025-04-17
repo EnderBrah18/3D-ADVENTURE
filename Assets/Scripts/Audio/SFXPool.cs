1102dfdf563e1899b867b5c29f7db29d0b8ff118
EnderBrah18 <jvictorguimadias@gmail.com>
2025-04-17 02:14:46 -0300

    Create Ambient Music And SFX Manager

---
+++ Assets/Scripts/Audio/SFXPool.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPool : MonoBehaviour
+{
+    private List<AudioSource> _audioSourceList;
+
+    public int poolSize = 10;
+
+    private int _index = 0;
+
+    private void Awake()
+    {
+        CreatePool();
+    }
+
+    private void CreatePool()
+    {
+        _audioSourceList = new List<AudioSource>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            CreateAudioSourceItem();
+        }
+    }
+
+    private void CreateAudioSourceItem()
+    {
+        GameObject go = new GameObject("SFX_Pool");
+        go.transform.SetParent(gameObject.transform);
+        _audioSourceList.Add(go.AddComponent<AudioSource>());
+    }
+
+    public void Play(SFXType sfxType)
+    {
+        var sfx = SoundManager.Instance.GetSFXByType(sfxType);
+
+        _audioSourceList[_index].clip = sfx.audioClip;
+        _audioSourceList[_index].Play();
+
+        _index++;
+        if(_index >= _audioSourceList.Count) _index = 0;
+    }
+}
--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ebac.Core.Singleton;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
     public List<MusicSetup> musicSetups;
     public List<SFXSetup> sfxSetups;
+
+    public AudioSource musicSource;
+
+    public void playMusicByType(MusicType musicType)
+    {
+        var music = GetMusicByType(musicType);
+        musicSource.clip = music.audioClip;
+        musicSource.Play();
+
+    }
+
+    public MusicSetup GetMusicByType(MusicType musicType)
+    {
+        return musicSetups.Find(i => i.musicType == musicType);
+    }
+
+    public SFXSetup GetSFXByType(SFXType sfxType)
+    {
+        return sfxSetups.Find(i => i.sfxType == sfxType);
+    }
+
 }
 
 public enum MusicType
